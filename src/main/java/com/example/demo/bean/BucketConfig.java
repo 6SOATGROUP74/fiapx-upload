ed9b461855f4edc970312869b46e3dd0064af7ad
igusvs <igoralves159@gmail.com>
2025-01-26 08:57:21 -0300

    Implementar beans local

--- src/main/java/com/example/demo/bean/BucketConfig.java
+++ src/main/java/com/example/demo/bean/BucketConfig.java
@@ -1,19 +1,34 @@
 package com.example.demo.bean;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Profile("prod")
 public class BucketConfig {
+
+    @Value("aws.accessKey")
+    private final String accessKey;
+
+    @Value("aws.secretKey")
+    private final String secretKey;
+
+    public BucketConfig(String accessKey, String secretKey) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
 
     @Bean
     public S3Client config() {
-
         return S3Client.builder().region(Region.US_EAST_1)
-                //.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .forcePathStyle(true).build();
 
     }
